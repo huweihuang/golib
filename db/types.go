db35cbf2bce1a43af911a870cbe08c6ea3f8b113
胡伟煌 <huweihuang@foxmail.com>
2023-10-18 17:19:47 +0800

    add gorm type (#5)

---
+++ db/types.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+// Scan for scanner helper
+func Scan(data interface{}, value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+	if len(bytes) == 0 {
+		return nil
+	}
+	return json.Unmarshal(bytes, data)
+}
+
+// Value for valuer helper
+func Value(data interface{}) (interface{}, error) {
+	vi := reflect.ValueOf(data)
+	// 判断是否为 0 值
+	if vi.IsZero() {
+		return nil, nil
+	}
+	return json.Marshal(data)
+}
+
+type GormMap map[string]string
+
+// Scan 实现 sql.Scanner 接口，Scan 将字符串变成结构体
+func (obj *GormMap) Scan(value interface{}) error {
+	return Scan(&obj, value)
+}
+
+// Value 实现 driver.Valuer 接口，Value 将结构体变成字符串
+func (obj GormMap) Value() (driver.Value, error) {
+	return Value(obj)
+}
