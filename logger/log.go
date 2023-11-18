d9cd173163a645a86702ecb81c0472f363686cd3
胡伟煌 <huweihuang@foxmail.com>
2023-11-18 11:56:57 +0800

    set default zap (#13)
    * set default zap
    
    * add enable color

--- logger/log.go
+++ logger/log.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"github.com/sirupsen/logrus"
+	"go.uber.org/atomic"
 	"go.uber.org/zap"
 
 	logrus2 "github.com/huweihuang/golib/logger/logrus"
@@ -14,23 +15,51 @@ const (
 )
 
 var (
-	ZapLogger    *zap.SugaredLogger
-	LogrusLogger *logrus.Logger
+	zapLogFilled    atomic.Bool
+	logrusLogFilled atomic.Bool
+
+	ZapSugaredLogger *zap.SugaredLogger
+	ZapLogger        *zap.Logger
+	LogrusLogger     *logrus.Logger
 )
 
 type LogConfig struct {
-	LogFile           string
-	LogLevel          string
-	LogFormat         string
-	EnableForceColors bool
+	LogFile     string
+	ErrorFile   string
+	LogLevel    string
+	LogFormat   string
+	EnableColor bool
 }
 
-func InitLogger(c *LogConfig, loggerType string) (*zap.SugaredLogger, *logrus.Logger) {
+func InitLogger(c *LogConfig, loggerType string) (*zap.Logger, *zap.SugaredLogger, *logrus.Logger) {
 	switch loggerType {
 	case ZapType:
-		_, ZapLogger = zap2.InitLogger(c.LogFile, c.LogLevel, c.LogFormat)
+		ZapLogger, ZapSugaredLogger = zap2.InitLogger(c.LogFile, c.ErrorFile, c.LogLevel, c.LogFormat, c.EnableColor)
+		zapLogFilled.Store(true)
 	case LogrusType:
-		LogrusLogger = logrus2.InitLogger(c.LogFile, c.LogLevel, c.LogFormat, c.EnableForceColors)
+		LogrusLogger = logrus2.InitLogger(c.LogFile, c.LogLevel, c.LogFormat, c.EnableColor)
+		logrusLogFilled.Store(true)
 	}
-	return ZapLogger, LogrusLogger
+	return ZapLogger, ZapSugaredLogger, LogrusLogger
+}
+
+func Logrus() *logrus.Logger {
+	if logrusLogFilled.Load() {
+		return LogrusLogger
+	}
+	return logrus2.InitDefaultLogger()
+}
+
+func Sugar() *zap.SugaredLogger {
+	if zapLogFilled.Load() {
+		return ZapSugaredLogger
+	}
+	return zap2.Logger()
+}
+
+func Zap() *zap.Logger {
+	if zapLogFilled.Load() {
+		return ZapLogger
+	}
+	return zap2.Log()
 }
